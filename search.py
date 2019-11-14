1a1b35078a05f596c87f74203aff900563ede63d
Rob Hoelz <rob@hoelz.ro>
2019-11-13 22:30:07 -0600

    Add search server

---
+++ search.py
@@ -0,0 +1,27 @@
+import contextlib
+import sqlite3
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.jinja_options.update(
+    variable_start_string='«',
+    variable_end_string='»',
+)
+# XXX reload templates
+
+@app.route('/')
+def search():
+    # XXX filename
+    conn = sqlite3.connect('/tmp/site/search-index.db')
+
+    # XXX handle no query
+    query = request.args.get('q', '')
+    with contextlib.closing(conn.cursor()) as c:
+        c.execute('select url, title from pages where pages match ? order by rank', (query,))
+        matches = c.fetchall()
+
+    return render_template('search.html', matches=matches)
+
+if __name__ == '__main__':
+    app.run()
