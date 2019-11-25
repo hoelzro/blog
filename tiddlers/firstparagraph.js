c2c394c4e371dbb549b971eb9eba244b2fdfecfa
Rob Hoelz <rob@hoelz.ro>
2019-11-25 11:02:25 -0600

    Add firstparagraph filter
    For extracting the first paragraph from a tiddler for the blog roll
    and RSS feed

---
+++ tiddlers/firstparagraph.js
@@ -0,0 +1,41 @@
+/*\
+title: $:/firstparagraph.js
+type: application/javascript
+module-type: filteroperator
+
+\*/
+
+(function() {
+    exports.firstparagraph = function(source, operand, options) {
+        let wiki = options.wiki;
+        let results = [];
+
+        source(function(_, title) {
+            let parser = wiki.parseTiddler(title);
+
+            if(!parser) {
+                return;
+            }
+
+            let newTree = [];
+
+            for(let node of parser.tree) {
+                if(node.type == 'element' && node.tag == 'p') {
+                    newTree.push(node);
+                    break;
+                }
+            }
+
+            parser.tree = newTree;
+
+
+            let widgetNode = wiki.makeWidget(parser, {});
+            let container = $tw.fakeDocument.createElement('div');
+            widgetNode.render(container, null);
+
+            results.push(container.innerHTML);
+        });
+
+        return results;
+    };
+})();
