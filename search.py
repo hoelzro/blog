a0ba068a551b03fd3c1cff9b6beea5351ac27c93
Rob Hoelz <rob@hoelz.ro>
2019-12-31 13:46:12 -0600

    Allow me to run the search daemon via waitress

--- search.py
+++ search.py
@@ -3,24 +3,28 @@ import sqlite3
 
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
-app.jinja_options.update(
-    variable_start_string='«',
-    variable_end_string='»',
-)
-# XXX reload templates
+def create_app(*args):
+    app = Flask(__name__)
+    app.jinja_options.update(
+        variable_start_string='«',
+        variable_end_string='»',
+    )
+    # XXX reload templates
 
-@app.route('/search')
-def search():
-    conn = sqlite3.connect('/var/www/http/hoelz.ro/search-index.db')
+    @app.route('/search')
+    def search():
+        conn = sqlite3.connect('/var/www/http/hoelz.ro/search-index.db')
 
-    # XXX handle no query
-    query = request.args.get('q', '')
-    with contextlib.closing(conn.cursor()) as c:
-        c.execute('select url, title from pages where pages match ? order by rank', (query,))
-        matches = c.fetchall()
+        # XXX handle no query
+        query = request.args.get('q', '')
+        with contextlib.closing(conn.cursor()) as c:
+            c.execute('select url, title from pages where pages match ? order by rank', (query,))
+            matches = c.fetchall()
 
-    return render_template('search.html', matches=matches)
+        return render_template('search.html', matches=matches)
+
+    return app.wsgi_app(*args)
 
 if __name__ == '__main__':
+    app = create_app()
     app.run()
